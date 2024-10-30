0f6e346a2d1a1e124a8cf1f3fef6320722ab7b2c
aljonardddd <aljonard.delacruz123@gmail.com>
2024-10-30 23:41:41 +0800

    data folder

--- data/index.tsx
+++ data/index.tsx
@@ -8,7 +8,7 @@ export const leftLinks = [
 ]
 export const rightLinks = [
     {name:"Privacy", link:"https://about.google/?fg=1"},
-    {name:"Terms", link:"https://about.google/?fg=1"},  
+    {name:"Terms", link:"https://policies.google.com/terms?hl=en-PH&fg=1"},  
     {name:"Settings", link:"https://about.google/?fg=1"},
 ]
 
